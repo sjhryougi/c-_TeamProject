3b3a458ebac651ba7d9544f4b73e7e2e0396039d
weri0527 <39061748+weri0527@users.noreply.github.com>
2023-05-26 11:43:40 +0900

    Add files via upload

---
+++ Login/Form2.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Login
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbox_id = new System.Windows.Forms.TextBox();
+            this.txtbox_pwd = new System.Windows.Forms.TextBox();
+            this.btn_signin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "회원가입";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "아이디";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "비밀번호";
+            // 
+            // txtbox_id
+            // 
+            this.txtbox_id.Location = new System.Drawing.Point(118, 51);
+            this.txtbox_id.Name = "txtbox_id";
+            this.txtbox_id.Size = new System.Drawing.Size(145, 25);
+            this.txtbox_id.TabIndex = 5;
+            // 
+            // txtbox_pwd
+            // 
+            this.txtbox_pwd.Location = new System.Drawing.Point(118, 101);
+            this.txtbox_pwd.Name = "txtbox_pwd";
+            this.txtbox_pwd.Size = new System.Drawing.Size(145, 25);
+            this.txtbox_pwd.TabIndex = 6;
+            // 
+            // btn_signin
+            // 
+            this.btn_signin.Location = new System.Drawing.Point(28, 153);
+            this.btn_signin.Name = "btn_signin";
+            this.btn_signin.Size = new System.Drawing.Size(235, 55);
+            this.btn_signin.TabIndex = 7;
+            this.btn_signin.Text = "회원가입";
+            this.btn_signin.UseVisualStyleBackColor = true;
+            this.btn_signin.Click += new System.EventHandler(this.btn_signin_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 239);
+            this.Controls.Add(this.btn_signin);
+            this.Controls.Add(this.txtbox_pwd);
+            this.Controls.Add(this.txtbox_id);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbox_id;
+        private System.Windows.Forms.TextBox txtbox_pwd;
+        private System.Windows.Forms.Button btn_signin;
+    }
+}

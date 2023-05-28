31036dedc386ba3a723c17c2c7310caa1f7bf8a3
hyjeong99 <104803807+hyjeong99@users.noreply.github.com>
2023-05-28 17:26:04 +0900

    Edit1

---
+++ ChattingProgram/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ChattingProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("1", "1.png");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("2", 1);
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("3", 2);
+            this.txtAll = new System.Windows.Forms.RichTextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.옵션OToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.도움말HToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lstFriend = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnImage = new System.Windows.Forms.Button();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtAll
+            // 
+            this.txtAll.BackColor = System.Drawing.SystemColors.Window;
+            this.txtAll.Location = new System.Drawing.Point(169, 37);
+            this.txtAll.Name = "txtAll";
+            this.txtAll.ReadOnly = true;
+            this.txtAll.Size = new System.Drawing.Size(619, 360);
+            this.txtAll.TabIndex = 0;
+            this.txtAll.Text = "";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(169, 417);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(553, 21);
+            this.txtSend.TabIndex = 1;
+            this.txtSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSend_KeyDown);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(729, 415);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(59, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "전송";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1.png");
+            this.imageList1.Images.SetKeyName(1, "2.png");
+            this.imageList1.Images.SetKeyName(2, "3.png");
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일FToolStripMenuItem,
+            this.옵션OToolStripMenuItem,
+            this.도움말HToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일FToolStripMenuItem
+            // 
+            this.파일FToolStripMenuItem.Name = "파일FToolStripMenuItem";
+            this.파일FToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.파일FToolStripMenuItem.Text = "파일(F)";
+            // 
+            // 옵션OToolStripMenuItem
+            // 
+            this.옵션OToolStripMenuItem.Name = "옵션OToolStripMenuItem";
+            this.옵션OToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.옵션OToolStripMenuItem.Text = "옵션(O)";
+            // 
+            // 도움말HToolStripMenuItem
+            // 
+            this.도움말HToolStripMenuItem.Name = "도움말HToolStripMenuItem";
+            this.도움말HToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.도움말HToolStripMenuItem.Text = "도움말(H)";
+            // 
+            // lstFriend
+            // 
+            this.lstFriend.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lstFriend.HideSelection = false;
+            this.lstFriend.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.lstFriend.Location = new System.Drawing.Point(12, 37);
+            this.lstFriend.MultiSelect = false;
+            this.lstFriend.Name = "lstFriend";
+            this.lstFriend.Size = new System.Drawing.Size(151, 360);
+            this.lstFriend.SmallImageList = this.imageList1;
+            this.lstFriend.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.lstFriend.TabIndex = 5;
+            this.lstFriend.TileSize = new System.Drawing.Size(228, 50);
+            this.lstFriend.UseCompatibleStateImageBehavior = false;
+            this.lstFriend.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 100;
+            // 
+            // btnImage
+            // 
+            this.btnImage.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnImage.ImageIndex = 0;
+            this.btnImage.ImageList = this.imageList2;
+            this.btnImage.Location = new System.Drawing.Point(12, 415);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(151, 28);
+            this.btnImage.TabIndex = 6;
+            this.btnImage.UseVisualStyleBackColor = true;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "2.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnImage);
+            this.Controls.Add(this.lstFriend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtAll);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtAll;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 옵션OToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 도움말HToolStripMenuItem;
+        private System.Windows.Forms.ListView lstFriend;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.ImageList imageList2;
+    }
+}
+
---
+++ ChattingProgram/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ChattingProgram
+{
+    public partial class Form1 : Form
+    {
+        public NetworkStream m_Stream;
+        public StreamReader m_Read;
+        public StreamWriter m_Write;
+        const int PORT = 2002;
+        private Thread m_ThReader;
+
+        public bool m_bStop = false;
+
+        private TcpListener m_listener;
+        private Thread m_thServer;
+
+        public bool m_bConnect = false;
+        TcpClient m_Client;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Message(string msg)
+        {
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                txtAll.AppendText(msg + "\r\n");
+
+                txtAll.Focus();
+                txtAll.ScrollToCaret();
+
+                txtSend.Focus();
+            }));
+        }
+
+        public void ServerStart()
+        {
+            try
+            {
+                m_listener = new TcpListener(PORT);
+                m_listener.Start();
+
+                m_bStop = true;
+                Message("클라이언트 접속 대기중");
+
+                while (m_bStop)
+                {
+                    TcpClient hClient = m_listener.AcceptTcpClient();
+
+                    if (hClient.Connected)
+                    {
+                        m_bConnect = true;
+                        Message("클라이언트 접속");
+
+                        m_Stream = hClient.GetStream();
+                        m_Read = new StreamReader(m_Stream);
+                        m_Write = new StreamWriter(m_Stream);
+
+                        m_ThReader = new Thread(new ThreadStart(Receive));
+                        m_ThReader.Start();
+                    }
+                }
+            }
+            catch
+            {
+                Message("시작 도중에 오류 발생");
+                return;
+            }
+        }
+
+        public void ServerStop()
+        {
+            if (!m_bStop)
+                return;
+
+            m_listener.Stop();
+
+            m_Read.Close();
+            m_Write.Close();
+
+            m_Stream.Close();
+
+            m_ThReader.Abort();
+            m_thServer.Abort();
+
+            Message("서비스 종료");
+        }
+
+        public void Disconnect()
+        {
+
+        }
+
+        public void Connect()
+        {
+
+        }
+
+        public void Receive()
+        {
+            try
+            {
+                while (m_bConnect)
+                {
+                    string szMessage = m_Read.ReadLine();
+
+                    if (szMessage != null)
+                        Message("상대방 >>> : " + szMessage);
+                }    
+            }
+            catch
+            {
+                Message("데이터를 읽는 과정에서 오류가 발생");
+            }
+            Disconnect();
+        }
+
+        public void Send()
+        {
+            try
+            {
+                m_Write.WriteLine(txtSend.Text);
+                m_Write.Flush();
+
+                Message(">>> : " + txtSend.Text);
+                txtSend.Text = "";
+            }
+            catch
+            {
+                Message("데이터 전송 실패");
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+
+        private void txtSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                Send();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 폼 로드 시 친구 목록 불러오기
+            LoadFriendsFromServer();
+        }
+
+        private void LoadFriendsFromServer()
+        {
+            // 서버에서 친구 목록을 가져오는 코드 작성
+            // 가져온 친구 목록을 lstFriend에 추가
+            
+        }
+    }
+}

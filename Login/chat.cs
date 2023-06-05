dc47924f75cb3abbe82c4778e25f7fd586620dfd
hyjeong99 <104803807+hyjeong99@users.noreply.github.com>
2023-06-06 00:34:27 +0900

    채팅 내용 갱신 수정

--- Login/chat.cs
+++ Login/chat.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
@@ -20,12 +22,16 @@ namespace ChattingProgram
         string myID;
         public string friendID;
 
+        Timer timer = new Timer();
+
         public chat(String Data)
         {
             InitializeComponent();
 
             connection.Open();
             myID = Data;
+            
+            timer.Interval = 500;
         }
         public void Send()
         {
@@ -35,21 +41,31 @@ namespace ChattingProgram
                 string sendTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
 
                 // 메시지 전송
-                try
-                {
-                    string insertMessage = string.Format("INSERT INTO chat (sender_id, receiver_id, content, time) VALUES ('{0}', '{1}', '{2}', '{3}');", myID, friendID, sendMessage, sendTime);
-                    MySqlCommand command = new MySqlCommand(insertMessage, connection);
-                    command.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("중복되는 텍스트이거나 텍스트의 길이가 너무 깁니다.");
-                }
+                timer.Stop();
+                string insertMessage = string.Format("INSERT IGNORE INTO chat (sender_id, receiver_id, content, time) VALUES ('{0}', '{1}', '{2}', '{3}');", myID, friendID, sendMessage, sendTime);
+                MySqlCommand command = new MySqlCommand(insertMessage, connection);
+                command.ExecuteNonQuery();
                 
-
                 txtSend.Clear();
+                timer.Start();
+                chatRefresh();
             }
         }
+
+        /*private static DateTime Delay(int MS)
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = DateTime.Now;
+            }
+
+            return DateTime.Now;
+        }*/
 
         private void btnSend_Click(object sender, EventArgs e)
         {
@@ -93,17 +109,40 @@ namespace ChattingProgram
 
         private void lstFriend_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // 친구 목록에서 선택할 경우
             if (lstFriend.SelectedItems.Count != 0)
             {
                 Clear();
+                chatRefresh();
+                timeRefresh();
+            }
+        }
 
-                friendID = lstFriend.Items[lstFriend.FocusedItem.Index].SubItems[0].Text.ToString();
-                //채팅 로그 가져오기
-                string getChatList = string.Format("SELECT * FROM chat WHERE sender_id = '{0}' or sender_id = '{1}' ORDER BY time ASC;", myID, friendID);
-                MySqlCommand command = new MySqlCommand(getChatList, connection);
-                MySqlDataReader chatReader = command.ExecuteReader();
+        private void timeRefresh()
+        {
+            // 0.5초마다 갱신
+            timer.Tick += new EventHandler(Timer_Tick);
+            //timer.Start();
+        }
 
-                while (chatReader.Read())
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            chatRefresh();
+        }
+
+        private void chatRefresh()
+        {
+            // 채팅창 갱신
+            friendID = lstFriend.Items[lstFriend.FocusedItem.Index].SubItems[0].Text.ToString();
+            // 채팅 로그 가져오기
+            string getChatList = string.Format("SELECT * FROM chat WHERE sender_id = '{0}' or sender_id = '{1}' ORDER BY time ASC;", myID, friendID);
+            MySqlCommand command = new MySqlCommand(getChatList, connection);
+            MySqlDataReader chatReader = command.ExecuteReader();
+
+            while (chatReader.Read())
+            {
+
+                if (!txtAll.Text.Contains(chatReader["time"].ToString()))
                 {
                     if (chatReader["sender_id"].ToString() == myID)
                     {
@@ -114,13 +153,14 @@ namespace ChattingProgram
                         txtAll.AppendText(friendID + ">>" + chatReader["content"].ToString() + "\n" + chatReader["time"].ToString() + "\n");
                     }
                 }
-                chatReader.Close();
             }
+            chatReader.Close();
         }
 
         private void chat_FormClosed(object sender, FormClosedEventArgs e)
         {
             connection.Close();
+            timer.Stop();
         }
     }
 }
